14981a811390a074f6b7aeb764a459bd3698a99c
stasya0903 <48295103+stasya0903@users.noreply.github.com>
2020-09-03 18:54:22 -0500

    added the name field and styles

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,92 +1,157 @@
 <template>
-    <form @submit.prevent="someAction()">
-     <div class="personal">
-      <input type="text" v-model="">
-     </div>
-        <div>
-            <input type="text" v-model="passportData">
-            <span v-if="$v.passportData.$invalid">
-        Серия и номер паспорта должны быть в формате 1234 567890
-      </span>
+    <div class="container">
+        <div class="formCard">
+            <div class="formCard_header">
+                <p>Добавить Клиента</p>
+            </div>
+            <form @submit.prevent="someAction" class="form">
+                <div class="formLine">
+                    <div class="formGroup">
+                        <div class="formGroup_input">
+                            <label class="textInputLabel" for="lastName">Фамилия</label>
+                            <input class="textInput"
+                                   id="lastName" type="text"
+                                   v-model.trim="$v.client.lastName.$model"
+                                   @blur="$v.client.lastName.$touch()"
+                                   :class="{ 'errorInput': $v.client.lastName.$error }">
+                        </div>
+
+                        <div v-if="$v.client.lastName.$error" class="inputError">
+                            Поле обязательно для заполнения
+                        </div>
+                    </div>
+                    <div class="formGroup">
+                        <div class="formGroup_input">
+                            <label class="textInputLabel" for="name">Имя</label>
+                            <input class="textInput"
+                                   id="name" type="text"
+                                   v-model.trim="$v.client.name.$model"
+                                   @blur="$v.client.name.$touch()"
+                                   :class="{ 'errorInput': $v.client.name.$error }">
+                        </div>
+                        <div v-if="$v.client.name.$error" class="inputError">
+                            Поле обязательно для заполнения
+                        </div>
+
+
+                    </div>
+                    <div class="formGroup">
+                        <div class="formGroup_input">
+                            <label class="textInputLabel" for="secondName">Отчество</label>
+                            <input class="textInput"
+                                   id="secondName"
+                                   type="text"
+                                   v-model.trim="$v.client.secondName.$model"
+                                   @blur="$v.client.secondName.$touch()"
+                                   :class="{ 'errorInput': $v.client.secondName.$error }">
+                        </div>
+                        <div v-if="$v.client.secondName.$error" class="inputError">
+                            Поле обязательно для заполнения
+                        </div>
+                    </div>
+
+                </div>
+                <div class="formLine">
+                    <div class="formGroup">
+                        <div class="formGroup_input">
+                            <label class="textInputLabel" for="dateOfBirth">Дата рождения</label>
+                            <input class="textInput"
+                                   id="dateOfBirth" type="date"
+                                   v-model.trim="$v.client.dateOfBirth.$model"
+                                   @blur="$v.client.dateOfBirth.$touch()"
+                                   :class="{ 'errorInput': $v.client.dateOfBirth.$error }">
+                        </div>
+
+                        <div v-if="$v.client.dateOfBirth.$error" class="inputError">
+                            Поле обязательно для заполнения
+                        </div>
+                    </div>
+                </div>
+
+                <!-- <div>
+                     <input type="text" v-model="passportData">
+                     <span v-if="$v.passportData.$invalid">
+         Серия и номер паспорта должны быть в формате 1234 567890
+       </span>
+                 </div>-->
+
+
+                <!--
+
+
+                  $v.passportDate.$model - объект, при записи данных в который:
+                  - Vuelidate присвоит переданное значение полю passportDate
+                  - Vuelidate вызовет метод $touch() у объекта $v.passportDate
+
+
+                -->
+                <!-- <div>
+                     <input type="text" v-model.lazy="$v.passportDate.$model">
+                     <span v-if="$v.passportDate.$error">
+         Дата должна быть в формате ДД.ММ.ГГГГ
+       </span>
+                 </div>-->
+                <!--:disabled="$v.$invalid"-->
+                <!-- Поле с несколькими ошибками -->
+
+
+                <button type="submit">
+                    Отправить форму
+                </button>
+            </form>
         </div>
-        <div>
-            <input type="text" v-model="passportDate" @blur="$v.passportDate.$touch()">
-            <span v-if="$v.passportDate.$error">
-        Дата должна быть в формате ДД.ММ.ГГГГ
-      </span>
-        </div>
+    </div>
 
-
-        <!--
-          Поле, которое тоже выведет ошибку после события blur, но с другим подходом
-
-          $v.passportDate.$model - объект, при записи данных в который:
-          - Vuelidate присвоит переданное значение полю passportDate
-          - Vuelidate вызовет метод $touch() у объекта $v.passportDate
-
-          Модификатор lazy необходим, чтобы присваивание произошло только после blur
-        -->
-        <div>
-            <input type="text" v-model.lazy="$v.passportDate.$model">
-            <span v-if="$v.passportDate.$error">
-        Дата должна быть в формате ДД.ММ.ГГГГ
-      </span>
-        </div>
-
-        <!-- Поле с несколькими ошибками -->
-        <div>
-            <input type="text" v-model="name" @blur="$v.name.$touch()">
-            <span v-if="$v.name.$error">
-        <template v-if="!$v.name.maxLength">
-          Длина имени не должна превышать {{ $v.name.$params.maxLength.max }} символов
-        </template>
-        <template v-else-if="!$v.name.alpha">
-          Имя должно содержать только буквы
-        </template>
-        <template v-else>
-          Имя обязательно для заполнения
-        </template>
-      </span>
-        </div>
-
-        <button type="submit" :disabled="$v.$invalid">
-            Отправить форму
-        </button>
-    </form>
 </template>
 
 <script>
-    import {required, maxLength} from 'vuelidate/lib/validators';
+    import {required} from 'vuelidate/lib/validators';
     import moment from 'moment';
 
     export default {
         data() {
             return {
-                passportData: null,
-                name: null,
-                passportDate: null,
+                client: {
+                    passportData: null,
+                    name: '',
+                    lastName: '',
+                    secondName: '',
+                    passportDate: '',
+                    dateOfBirth:''
+                }
             };
         },
         validations: {
-            // Название поля должно совпадать с полем в data
-            passportData: {
-                required,
-                validFormat: val => /^\d{4} \d{6}$/.test(val),
-            },
-            passportDate: {
-                required,
-                validDate: val => moment(val, 'DD.MM.YYYY', true).isValid(),
-            },
-            name: {
-                required,
-                maxLength: maxLength(10),
-                alpha: val => /^[а-яё]*$/i.test(val),
+            client: {
+                passportData: {
+                    required,
+                    validFormat: val => /^\d{4} \d{6}$/.test(val),
+                },
+                passportDate: {
+                    required,
+                    validDate: val => moment(val, 'DD.MM.YYYY', true).isValid(),
+                },
+                name: {
+                    required,
+                },
+                lastName: {
+                    required,
+                },
+                secondName: {
+                    required,
+                },
+                dateOfBirth:{
+                    required,
+                  /*  date_format: 'dd.MM.yyyy'*/
+                }
+
             },
         },
-     someAction(){
-
-     }
-
+        methods: {
+            someAction() {
+                console.log(this.client)
+            },
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuelidate from 'vuelidate'
+import '../public/style.css';
+
 
 
 Vue.use(Vuelidate);
