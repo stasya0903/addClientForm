623bf2591672c1b5235182de5ce55aada7199bf8
stasya0903 <48295103+stasya0903@users.noreply.github.com>
2020-09-04 11:51:04 -0500

    added separted component for the input type text

---
+++ src/components/InputText.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="formGroup">
+        <div class="formGroup_input">
+            <label class="textInputLabel" :for="name">{{label}}
+                <span v-if="validation.$params.required" class="required">*</span></label>
+            <input type="text"
+                   class="textInput"
+                   :placeholder="placeholder"
+                   :id="name"
+                   v-model.trim="validation.$model"
+                   @blur="makeValidation($event.target.value)"
+                   :class="{ 'errorInput': validation.$error }">
+        </div>
+        <div v-if="validation.$error" class="inputError">
+            <template v-if="paramsArray.includes('required')">
+                <p v-show="!validation.required">{{errorMsg['required']}}</p>
+            </template>
+                <template v-if="validation.$model.length > 0">
+                    <template v-for="param in paramsArray.filter((element)=> element !=='required')">
+                        <p v-if="(!validation[`${param}`])" :key='param'>{{errorMsg[`${param}`]}}</p>
+                    </template>
+                </template>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        name: "InputText",
+        props: {
+            label: String,
+            placeholder: String,
+            name: String,
+            validation: Object,
+        },
+        data() {
+            return {
+                value: '',
+                errorMsg: {
+                    required: "Поле обязательно для заполнения",
+                    phoneNumber: "Введите телефон в формате +7**********",
+                    alpha: `${this.$props.label} может содержать только буквы`
+                },
+                paramsArray: [...Object.keys(this.validation.$params)]
+            }
+        },
+        methods: {
+            makeValidation(value) {
+                this.$emit('setValue', value);
+                this.validation.$touch();
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
