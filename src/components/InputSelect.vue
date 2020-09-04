f22283a3a1ddd8c7d5ef6a661e5008f8febf7af0
stasya0903 <48295103+stasya0903@users.noreply.github.com>
2020-09-04 18:48:18 -0500

    added component for input select

---
+++ src/components/InputSelect.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="formGroup">
+        <div class="formGroup_input">
+            <label class="textInputLabel" :for="name">{{label}}
+                <span v-if="validation.$params.required" class="required">*</span></label>
+            <div class="selectInput"
+                 @click="openSelect()"
+                 :class="{ 'errorInput': validation.$error && !showSelect }">
+                <div class="optionsSelected">
+                    <span class="optionDefault" v-if="validation.$model.length === 0">Выберите опцию...</span>
+                    <template v-else>
+                        <span class="selectedOptions" :key="item.index"
+                              v-for="item in validation.$model">{{item}}</span>
+                    </template>
+                </div>
+                <div class="selectedArrow">
+                    <img v-if="!showSelect" class="selectArrow_img" src="../assets/arrowDown.svg">
+                    <img v-if="showSelect" class="selectArrow_img" src="../assets/up-arrow.svg">
+                </div>
+            </div>
+            <select v-show="showSelect"
+                    :multiple="mult"
+                    :size="$props.options.length"
+                    :id="name"
+                    v-model.trim="validation.$model"
+                    @input="setValue($event.target.value)
+"
+            >
+                <option v-for="option of this.$props.options"
+                        :value="option"
+                        :key="option"
+                        class="selectOption">{{option}}
+                </option>
+            </select>
+        </div>
+        <div v-if="this.validation.$error && !showSelect" class="inputError">
+            {{$props.errorMsg['required']}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        name: "InputSelect",
+        props: {
+            label: String,
+            name: String,
+            validation: Object,
+            options: Array,
+            mult: Boolean,
+            errorMsg:Array
+        },
+        data() {
+            return {
+                showSelect: false,
+                values: [],
+                paramsArray: [...Object.keys(this.validation.$params)]
+            }
+        },
+        methods: {
+            setValue(value) {
+                this.$emit('setValue', value);
+
+            },
+            openSelect(){
+                this.showSelect = !this.showSelect;
+             this.validation.$touch()
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
